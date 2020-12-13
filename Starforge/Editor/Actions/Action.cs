20c19a3bbf141e0b81e419b3822f7cad75d081c2
Anton Reinhard <s1509337@msx.tu-dresden.de>
2020-12-13 17:30:03 +0100

    Add Actions and TilePlacement Classes

---
+++ Starforge/Editor/Actions/Action.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Starforge.MapStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starforge.Editor.Actions {
+
+    // representing a user action, like placing/removing an entity or a tile
+    abstract class Action {
+
+        // the type of action
+        private ToolType toolType;
+
+        // the place of the action
+        private Rectangle position;
+
+        public Action() {
+
+        }
+
+        public abstract void Apply(Level l);
+    }
+
+}
---
+++ Starforge/Editor/Actions/TilePlacement.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Starforge.MapStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starforge.Editor.Actions {
+
+    class TilePlacement : Action {
+
+        private TileType tileType;
+
+        private Rectangle area;
+
+        public TilePlacement(TileType t, Rectangle a) {
+            tileType = t;
+            area = a;
+        }
+
+        public override void Apply(Level l) {
+            throw new NotImplementedException();
+        }
+    }
+}
