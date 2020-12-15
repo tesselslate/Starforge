e0c1365adeae7664f127522938e41bbb7989d606
WoofWoofDoggo <46545045+WoofWoofDoggo@users.noreply.github.com>
2020-12-15 10:07:45 -0500

    Include nativefiledialog + C# wrapper (#6)
    * NativeFileDialog wrapper
    
    * Include nativefiledialog
    
    Set up project to include nativefiledialog when building.
    
    * Move FNA and NFD to lib folder
    
    * Fix crash with what is likely a memory leak :)

--- README.md
+++ README.md
@@ -8,7 +8,18 @@ Starforge is an experimental map editor for Celeste. **It is still in very early
 
 ---
 # Compiling from Source
-To compile from source, you can clone this repository and then build using Visual Studio or the toolkit of your choice. Make sure to recursively clone submodules and restore NuGet packages if necessary. You will need a build of FNA and its associated libraries to run Starforge. FNA is provided as a submodule which you can build yourself, and the native libraries can be found on [the wiki.](https://fna-xna.github.io/download/) Place the native libraries in the output directory containing `Starforge.exe`.
+First, clone the Git repository and its submodules (you can do the latter with `git submodule update --init --recursive`).
+
+### Visual Studio:
+- Compile FNA from source (make sure you are **creating a release build**) within the included submodule, which is located in the `lib` folder. The Starforge project is set up to automatically pull it from the `bin` folder.
+- Download the FNA [native libraries](http://fna.flibitijibibo.com/archive/fnalibs.tar.bz2) and unpack them. You will need to place **the x86 versions** of `FNA3D.dll` and `SDL2.dll` within the Starforge output directory (where the final `exe` is located.)
+
+### If you are not using Visual Studio:
+- Restore NuGet packages. Starforge requires `ImGui.NET`. Both `ImGui.NET.dll` and `cimgui.dll` should be present in the output directory containing the final executable.
+- Compile the included nativefiledialog fork, and FNA submodule (both located in `lib`). Place the resulting DLLs into the output directory.
+- Acquire the appropriate [native libraries](http://fna.flibitijibibo.com/archive/fnalibs.tar.bz2) for FNA and place them in the output directory.
+
+These steps may differ and libraries may not work depending on the platform you are using. If you have problems attempting to build the project, please open an issue so we can attempt to help and update these instructions to better reflect how to do so.
 
 ---
 # Contributing
@@ -19,6 +30,7 @@ Public contributions are currently not accepted. You are, however, welcome to cr
 Starforge is licensed under the [MIT License.](https://github.com/WoofWoofDoggo/Starforge/blob/main/LICENSE) It also makes use of various open source projects, whose licenses can be found below.
 
 - [FNA](https://github.com/FNA-XNA/FNA), which is under the [Microsoft Public License.](https://github.com/FNA-XNA/FNA/blob/master/licenses/LICENSE)
-- [ImGUI.NET](https://github.com/mellinoe/ImGui.NET), which is under the [MIT License.](https://github.com/mellinoe/ImGui.NET/blob/master/LICENSE)
+- [ImGui.NET](https://github.com/mellinoe/ImGui.NET), which is under the [MIT License.](https://github.com/mellinoe/ImGui.NET/blob/master/LICENSE)
 
+#### Additionally, Starforge makes use of a [fork](https://github.com/WoofWoofDoggo/nativefiledialog) of [nativefiledialog](https://github.com/mlabbe/nativefiledialog), as well as a C# wrapper for it based off of [nfd-sharp.](https://github.com/benklett/nfd-sharp)
 ---
---
+++ Starforge/Core/Interop/NFD.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+// This wrapper is heavily based off of the nfd-sharp project.
+// It can be found here: https://github.com/benklett/nfd-sharp/blob/master/NfdSharp/Nfd.cs
+//                       https://github.com/benklett/nfd-sharp/blob/master/NfdSharp/Utils.cs
+
+namespace Starforge.Core.Interop {
+    public static class NFD {
+        public enum NfdResult {
+            NFD_ERROR,
+            NFD_OKAY,
+            NFD_CANCEL
+        }
+
+        [DllImport("nfd_d", CallingConvention = CallingConvention.Cdecl)]
+        private static extern NfdResult NFD_OpenDialog(
+            IntPtr filterList,
+            IntPtr defaultPath,
+            out IntPtr outPath);
+
+        [DllImport("nfd_d", CallingConvention = CallingConvention.Cdecl)]
+        private static extern NfdResult NFD_SaveDialog(
+            IntPtr filterList,
+            IntPtr defaultPath,
+            out IntPtr outPath);
+
+        [DllImport("nfd_d", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr NFD_GetError();
+
+        public static NfdResult OpenDialog(string filterList, string defaultPath, out string path) {
+            IntPtr filterListPtr = NFDParser.ToNfdString(filterList);
+            IntPtr defaultPathPtr = NFDParser.ToNfdString(defaultPath);
+
+            NfdResult res = NFD_OpenDialog(filterListPtr, defaultPathPtr, out IntPtr outPath);
+            Marshal.FreeHGlobal(filterListPtr);
+            Marshal.FreeHGlobal(defaultPathPtr);
+
+            path = res != NfdResult.NFD_OKAY ? null : NFDParser.FromNfdString(outPath);
+
+            // Here, we *should* free the outPath pointer.
+            // However, doing so causes a crash!
+            // Does not doing so cause a memory leak? Probably. :)
+
+            if (res == NfdResult.NFD_ERROR) {
+                Logger.Log(LogLevel.Error, "nativefiledialog error:");
+                Logger.Log(LogLevel.Error, GetError());
+            }
+
+            return res;
+        }
+
+        public static NfdResult SaveDialog(string filterList, string defaultPath, out string path) {
+            IntPtr filterListPtr = NFDParser.ToNfdString(filterList);
+            IntPtr defaultPathPtr = NFDParser.ToNfdString(defaultPath);
+
+            NfdResult res = NFD_SaveDialog(filterListPtr, defaultPathPtr, out IntPtr outPath);
+            Marshal.FreeHGlobal(filterListPtr);
+            Marshal.FreeHGlobal(defaultPathPtr);
+
+            path = res != NfdResult.NFD_OKAY ? null : NFDParser.FromNfdString(outPath);
+
+            // The outPath pointer should also probably be freed here.
+
+            if (res == NfdResult.NFD_ERROR) {
+                Logger.Log(LogLevel.Error, "nativefiledialog error:");
+                Logger.Log(LogLevel.Error, GetError());
+            }
+
+            return res;
+        }
+
+        public static string GetError() {
+            return NFDParser.FromNfdString(NFD_GetError());
+        }
+    }
+
+    public static class NFDParser {
+        public static IntPtr ToNfdString(string str) {
+            if (str == "") return IntPtr.Zero;
+
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            byte[] nullTerm = new byte[bytes.Length + 1];
+            Array.Copy(bytes, nullTerm, bytes.Length);
+            nullTerm[nullTerm.Length - 1] = 0; // Null terminate string
+
+            // Create IntPtr for null-terminated string
+            IntPtr ptr = Marshal.AllocHGlobal(nullTerm.Length);
+            Marshal.Copy(nullTerm, 0, ptr, nullTerm.Length);
+
+            return ptr;
+        }
+
+        public static string FromNfdString(IntPtr ptr) {
+            byte[] bytes = new byte[4096];
+
+            // Copy bytes from pointer
+            Marshal.Copy(ptr, bytes, 0, 1);
+            int i = 0;
+
+            // While string is not null terminated, copy bytes
+            while(i < 4095 && bytes[i++] != 0) {
+                Marshal.Copy(ptr, bytes, 0, i + 1);
+            }
+
+            // Decrement index to get string length
+            i--;
+
+            byte[] res = new byte[i];
+            Array.Copy(bytes, res, i);
+
+            return Encoding.UTF8.GetString(res);
+        }
+    }
+}
